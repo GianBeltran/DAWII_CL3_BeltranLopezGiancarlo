3ca519c6f66c88006cc447a3be53f613ab4a7b8b
GianBeltran <Gbeltranl0506@gmail.com>
2024-07-03 16:12:19 -0500

    Avance pregunta 3

--- src/app/principal/fotos/fotos.component.ts
+++ src/app/principal/fotos/fotos.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../angular-material/material/material.module';
+import { CommonModule } from '@angular/common';
+import { Foto } from './foto';
+import { FotosService } from './fotos.service';
 
 @Component({
   selector: 'app-fotos',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule, CommonModule],
   templateUrl: './fotos.component.html',
   styleUrl: './fotos.component.css'
 })
 export class FotosComponent {
+
+  fotos: Foto[] = [];
+
+  constructor(private fotosService:FotosService) { }
+
+  ngOnInit(): void {
+    this.fotosService.getFotos().subscribe(data => {
+      console.log(data)
+      this.fotos = data;
+    });
+  }
 
 }
--- src/app/principal/fotos/fotos.service.ts
+++ src/app/principal/fotos/fotos.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { Foto } from './foto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FotosService {
 
-  constructor() { }
+  private url = 'https://jsonplaceholder.typicode.com/photos';
+
+  constructor(private httpClient:HttpClient) { }
+
+  getFotos(): Observable<Foto[]>{
+    return this.httpClient.get<Foto[]>(this.url).pipe(map(fotos => fotos.filter(foto => /^a|^r|^s/i.test(foto.title))));
+  }
 }
